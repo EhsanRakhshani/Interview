9179cf6fa785e293ca16410793640b02a24f8e1b
E_rakhshani89@hotmail.com <E_rakhshani89@hotmail.com>
2021-09-13 11:39:03 +0430

    init

---
+++ Interview/Interview.OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interview.OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
